43f22dae70305923805534de724eea8ae6594c8d
jcastaneda30 <jaidercasta30@gmail.com>
2023-02-24 21:57:07 -0500

    Version tre

---
+++ src/main/java/taller3--/televisores/Marca.java
@@ -0,0 +1,15 @@
+package taller3.televisores;
+
+public class Marca {
+	private String nombre;
+	
+	public Marca(String nom) {
+		nombre=nom;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String name) {
+		nombre=name;
+	}
+}
